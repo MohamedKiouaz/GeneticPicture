2df784f844f912c31d4f9bdf6c880b7e9293a748
indetronable <mohamed.kiouaz@gmail.com>
2017-06-22 11:56:53 +0100

    Little

--- GeneticsPicture.m
+++ GeneticsPicture.m
@@ -16,9 +16,9 @@ end
 [c d] = size(img);
 
 P = 2;
-Generations = 1000;
+G = 1000;
 N = 800;
-nb_enfants = 3;
+child = 3;
 
 if(exist(savefile, 'file') == 2)
     load(savefile)
@@ -35,9 +35,9 @@ imshow(img)
 
 img_clean = ones([c d], 'uint8') .* mean(mean(img));
 
-for i=0:Generations
+for i=0:G
     [a b] = size(people);
-    for j=1:nb_enfants*a
+    for j=1:child*a
         x_temp = people(mod(j, a) + 1, :, :);
         x_temp = reshape(x_temp, N, 5);
         img3 = img_clean;
@@ -45,8 +45,8 @@ for i=0:Generations
            img3(x_temp(k, 1):x_temp(k, 3), x_temp(k, 2):x_temp(k, 4)) = x_temp(k, 5);
         end
         for k=1:N
-            if(rand() < 1/N/4)
-                l = randi(50) + 10;
+            if(rand() < 4/N)
+                l = randi(25) + 5;
                 x_temp(k, 1) = randi(c - l);
                 x_temp(k, 2) = randi(d - l);
                 x_temp(k, 3:4) = x_temp(k, 1:2) + l;
@@ -69,7 +69,7 @@ for i=0:Generations
         title(strcat("Distance=", num2str(f)))
         
         subplot(2, 2, [3 4])
-        if(size(D, 2) > 1000 && mod(size(D, 2), 2) == 0)
+        if(size(D, 2) > 5000 && mod(size(D, 2), 2) == 0)
             D = mean([D(1, 1:2:end)', D(1, 2:2:end)'], 2)';
         end
         plot(D)
